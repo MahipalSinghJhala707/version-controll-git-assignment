961b4cfefaaebe7adf503ad86de3c78b3f6f1ae2
MahipalSinghJhala707 <mahipalsinghjhala707@gmail.com>
2026-01-18 13:44:02 +0530

    Feat: add backend api for storage of data in db

--- to-do/app.py
+++ to-do/app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask , request
+from dotenv import load_dotenv
+import os
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+load_dotenv()
+
+uri = os.getenv("MONGO_URI")
+
+client = MongoClient(uri, server_api=ServerApi("1"))
+db = client.users
+collection = db["items"]
+
 
 
 
 app = Flask(__name__)
 
 
+app.route('/submittodoitem', methods=['POST'])
+def submit():
+    item_name = request.form.get("itemName")
+    item_description = request.form.get("itemDescription")
 
+    collection.insert_one({
+        "itemName": item_name,
+        "itemDescription": item_description
+    })
+    return "Item Stored Successfully"
 
 
 if __name__ == "__main__":
